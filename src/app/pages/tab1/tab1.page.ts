433078b1bc9b5c3e18fc58dcc21a6e3a55160017
Shoma <sho1228.k@gmail.com>
2021-01-01 18:55:57 +0900

    add datas

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -9,17 +9,17 @@ import { MoneyItem } from 'src/interface';
 })
 export class Tab1Page {
   datas: MoneyItem[] = [
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date().toDateString()},
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date().toDateString()},
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date().toDateString()},
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,30).toDateString()},
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,28).toDateString()},
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,28).toDateString()},
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,25).toDateString()},
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,22).toDateString()},
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,22).toDateString()},
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,22).toDateString()},
-    {isIncome: true, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,20).toDateString()},
+    {isIncome: false, category: 'eat', memo: 'clothes', amount: 2000, date: new Date().toDateString()},
+    {isIncome: false, category: 'eat', memo: 'clothes', amount: 2000, date: new Date().toDateString()},
+    {isIncome: true, category: 'work', memo: '', amount: 300000, date: new Date().toDateString()},
+    {isIncome: false, category: 'eat', memo: 'clothes', amount: 500, date: new Date(2020,11,30).toDateString()},
+    {isIncome: false, category: 'eat', memo: 'clothes', amount: 1000, date: new Date(2020,11,28).toDateString()},
+    {isIncome: false, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,28).toDateString()},
+    {isIncome: false, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,25).toDateString()},
+    {isIncome: false, category: 'eat', memo: 'clothes', amount: 3000, date: new Date(2020,11,22).toDateString()},
+    {isIncome: false, category: 'eat', memo: 'clothes', amount: 600, date: new Date(2020,11,22).toDateString()},
+    {isIncome: false, category: 'eat', memo: 'clothes', amount: 800, date: new Date(2020,11,22).toDateString()},
+    {isIncome: false, category: 'eat', memo: 'clothes', amount: 3000, date: new Date(2020,11,20).toDateString()},
   ]
 
   constructor() {}
