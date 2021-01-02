2747b12e8479c80232a4712f0afa98f0aeea0322
Shoma <sho1228.k@gmail.com>
2021-01-02 12:25:28 +0900

    Write StorageService

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,9 +1,85 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { Store } from '@ngrx/store';
+import { MoneyItem } from 'src/interface';
+import { updateDetailItems, updateMaxAmount } from '../store/data';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
-  constructor() { }
+  constructor(
+    private storage: Storage,
+    private store: Store
+  ) { }
+
+  //
+  // detailItems
+  //
+
+  // from Home
+  loadDetailItems(){
+    return this.storage.get('detailItemData').then((res: MoneyItem[]) => {
+      this.store.dispatch(updateDetailItems({detailItems: res}));
+      return res;
+    })
+  }
+
+  // from income,expensees
+  // add item
+  pushDetailItem(item: MoneyItem){
+    this.storage.get('detailItemData').then((res: MoneyItem[]) => {
+      const detailItems = res.splice(0, 0, item);
+      this.storage.set('detailItemDeta',detailItems);
+      this.store.dispatch(updateDetailItems({detailItems}))
+    })
+  }
+
+  //  from detail
+  //  remove item
+  removeDetailItem(id: string){
+    this.storage.get('detailItemData').then((res: MoneyItem[]) => {
+      const detailItems = res.filter(item => item.id !== id);
+      this.storage.set('detailItemDeta',detailItems);
+      this.store.dispatch(updateDetailItems({detailItems}))
+    })
+  }
+
+  // from editDetail
+  // update item
+  updateDetailItem(updatedItem: MoneyItem){
+    this.storage.get('detailItemData').then((res: MoneyItem[]) => {
+      const detailItems = res.map(item => {
+        if(item.id === updatedItem.id){
+          return updatedItem;
+        }else{
+          return item;
+        }
+      })
+      this.storage.set('detailItemDeta',detailItems);
+      this.store.dispatch(updateDetailItems({detailItems}))
+    })
+  }
+
+
+  //
+  // maxAmount
+  //
+
+  // from Report
+  loadMaxAmount(){
+    return this.storage.get('maxAmount').then((res: number) => {
+      this.store.dispatch(updateMaxAmount({maxAmount: res}));
+      return res;
+    })
+  }
+
+  // from setting
+  // update maxAmount
+  updateMaxAmount(maxAmount: number){
+    this.storage.set('maxAmount',maxAmount);
+    this.store.dispatch(updateMaxAmount({maxAmount: maxAmount}));
+  }
 }
