d6ea9cfea6372aa27471b2b93db7af8dd695ad13
Shoma <sho1228.k@gmail.com>
2021-01-02 13:04:20 +0900

    add routing

--- src/app/pages/expenses/expenses.page.ts
+++ src/app/pages/expenses/expenses.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-expenses',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpensesPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  toHome(){
+    this.router.navigate(['/']);
   }
 
 }
--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MoneyItem } from 'src/interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -8,19 +9,27 @@ import { MoneyItem } from 'src/interface';
 })
 export class Tab1Page {
   datas: MoneyItem[] = [
-    {isIncome: false, category: 'eat', memo: 'clothes', amount: 2000, date: new Date().toDateString()},
-    {isIncome: false, category: 'eat', memo: 'clothes', amount: 2000, date: new Date().toDateString()},
-    {isIncome: true, category: 'work', memo: '', amount: 300000, date: new Date().toDateString()},
-    {isIncome: false, category: 'eat', memo: 'clothes', amount: 500, date: new Date(2020,11,30).toDateString()},
-    {isIncome: false, category: 'eat', memo: 'clothes', amount: 1000, date: new Date(2020,11,28).toDateString()},
-    {isIncome: false, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,28).toDateString()},
-    {isIncome: false, category: 'eat', memo: 'clothes', amount: 2000, date: new Date(2020,11,25).toDateString()},
-    {isIncome: false, category: 'eat', memo: 'clothes', amount: 3000, date: new Date(2020,11,22).toDateString()},
-    {isIncome: false, category: 'eat', memo: 'clothes', amount: 600, date: new Date(2020,11,22).toDateString()},
-    {isIncome: false, category: 'eat', memo: 'clothes', amount: 800, date: new Date(2020,11,22).toDateString()},
-    {isIncome: false, category: 'eat', memo: 'clothes', amount: 3000, date: new Date(2020,11,20).toDateString()},
+    {isIncome: false, category: 'eat', id: 'a', memo: 'clothes', amount: 2000, date: new Date().toDateString()},
+    {isIncome: false, category: 'eat', id: 'b', memo: 'clothes', amount: 2000, date: new Date().toDateString()},
+    {isIncome: true, category: 'work', id: 'c', memo: '', amount: 300000, date: new Date().toDateString()},
+    {isIncome: false, category: 'eat', id: 'd', memo: 'clothes', amount: 500, date: new Date(2020,11,30).toDateString()},
+    {isIncome: false, category: 'eat', id: 'e', memo: 'clothes', amount: 1000, date: new Date(2020,11,28).toDateString()},
+    {isIncome: false, category: 'eat', id: 'f', memo: 'clothes', amount: 2000, date: new Date(2020,11,28).toDateString()},
+    {isIncome: false, category: 'eat', id: 'g', memo: 'clothes', amount: 2000, date: new Date(2020,11,25).toDateString()},
+    {isIncome: false, category: 'eat', id: 'h', memo: 'clothes', amount: 3000, date: new Date(2020,11,22).toDateString()},
+    {isIncome: false, category: 'eat', id: 'i', memo: 'clothes', amount: 600, date: new Date(2020,11,22).toDateString()},
+    {isIncome: false, category: 'eat', id: 'j', memo: 'clothes', amount: 800, date: new Date(2020,11,22).toDateString()},
+    {isIncome: false, category: 'eat', id: 'k', memo: 'clothes', amount: 3000, date: new Date(2020,11,20).toDateString()},
   ]
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  toIncome(){
+    this.router.navigate(['/income']);
+  }
+
+  toExpenses(){
+    this.router.navigate(['/expenses']);
+  }
 
 }
